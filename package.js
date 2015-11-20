8194b8a1e2ebeada6c3cd46836c9ea1e1bb36ca4
Christian Fritz <cfritz@parc.com>
2015-11-20 14:15:39 -0800

    Added more loaders

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	summary: "Three.js core library + several useful extensions from the examples folder.",
-	version: "0.73.2",
+	version: "0.73.3",
 	name: "chfritz:three",
 	git: "https://github.com/chfritz/meteor-three.git",
 });
